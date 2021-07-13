9082a11953a01867526081816ed0cca02df477cc
Rafaela <rafaelazaccarias@gmail.com>
2021-07-13 18:22:21 -0300

    Adicionando Alertas

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 
@@ -19,7 +20,8 @@ export class EntrarComponent implements OnInit {
   constructor(
     private auth: AuthService,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit(){
@@ -50,7 +52,7 @@ export class EntrarComponent implements OnInit {
       this.router.navigate(['/postagem'])
     }, erro => {
       if (erro.status == 500) {
-        alert("Usuário ou senha incorretos")
+        this.alertas.showAlertDanger("Usuário ou senha incorretos")
       }
     })
 
